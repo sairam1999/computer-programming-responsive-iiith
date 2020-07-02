cc9a75ae025924ee18871f3cabd877244c760af9
sairam1999 <sairam2076@gmail.com>
2020-07-02 23:18:23 +0530

    CSE4_1_1 FIXED disabled manual textbox while considering Random values

--- src/lab/exp4/simulation/1-D/array.js
+++ src/lab/exp4/simulation/1-D/array.js
@@ -51,6 +51,8 @@ window.view = {
 		this.addClickEvent('btnOk', function() { view.proceedToStartButton() })
 		this.addClickEvent('btnStart', function() { view.displayElements() })
 		this.addClickEvent('btnNext', function() { view.sortArray() })
+		this.addClickEvent('btnRandom',function() { view.ToDisableTextBox()})
+		this.addClickEvent('btnManual',function() { view.ToEnableTextBox()})
 	},
 	proceedToStartButton: function() {
 		var userInput = this.getArraySize()
@@ -264,6 +266,12 @@ window.view = {
 		}
 		else
 			this.highlightNextStep()
+	},
+	ToDisableTextBox: function(){
+		document.getElementById("userInput").disabled = true;
+	},
+	ToEnableTextBox: function(){
+		document.getElementById("userInput").disabled = false;
 	},
 	init: function() {
 		this.activateEvents()
