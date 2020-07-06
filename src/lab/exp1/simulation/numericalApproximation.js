a68a35422861717930a65d553cfd611bb5811175
sairam1999 <sairam2076@gmail.com>
2020-07-06 23:33:35 +0530

    CSE1 FIXED week3 (range 1-10 for a,b values)

--- src/lab/exp1/simulation/numericalApproximation.js
+++ src/lab/exp1/simulation/numericalApproximation.js
@@ -290,8 +290,9 @@ window.view = {
 			alert('Enter numeric value of a and b');
 			return false;
 		} 
-		else if (valueA2 >= valueB2 || valueB2 > 30) {
-			alert('Integration Limits are from 0 to 30, b > a and b-a >= 1');
+		else if (valueA2 >= valueB2 || valueA2 < 1 || valueA2 > 10 || valueB2 < 1 || valueB2 > 10) {
+			alert('INVALID INPUT')
+			alert('Integration Limits are from 1 to 10, b > a and b-a >= 1');
 			return false;
 		}
 		else {
